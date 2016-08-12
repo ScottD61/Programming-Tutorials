5827375d0d2c80c877cd329eb75a9ecb7823873a
Scott Davis <ScDavis6@gmail.com>
2016-08-12 16:00:34 -0700

    HackerRank Python

---
+++ HackerRank -Python 3/Warmup Challenges/Solvemefirst.py
@@ -0,0 +1,12 @@
+Solvemefirst.py
+
+"""HackerRank challenge in python 3"""
+
+num1 = int(input())
+num2 = int(input())
+def solveMeFirst(a, b):
+    return(a + b)
+res = solveMeFirst(num1, num2)
+print(res)
+
+#Remember to put inputs outside of function
---
+++ HackerRank -Python 3/Warmup Challenges/Solvemesecond.py
@@ -0,0 +1,10 @@
+Solvemesecond.py
+
+def solveMeSecond(a,b):
+   return a+b
+n = int(input())
+for i in range(0,n):
+    a, b = input().split()
+    a,b = int(a),int(b)
+    res = solveMeSecond(a,b)
+    print (res)
