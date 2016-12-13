6549f39979e8eb2cdbcc2c081e96c9dc6c24597b
Scott Davis <ScDavis6@gmail.com>
2016-12-13 13:08:08 -0800

    Update

--- SQLZoo/4.SELECT with SELECT.sql
+++ SQLZoo/4.SELECT with SELECT.sql
@@ -8,3 +8,6 @@ SELECT name FROM world WHERE gdp/population > (SELECT gdp/population FROM world 
 
 #3 
 SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name;
+
+#4
+SELECT name, population FROM world  WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND  population < (SELECT population FROM world WHERE name = 'Poland');
